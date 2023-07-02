4768eef523c9de567b7548f1a69254e1e18045b5
shenqing <shenqin1@msu.edu>
2023-07-02 08:31:28 -0400

    Initial Commit

---
+++ LocationSearchSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationSearchSwiftUI
+//
+//  Created by 沈清昊 on 7/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationSearchSwiftUI/LocationSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchSwiftUIApp.swift
+//  LocationSearchSwiftUI
+//
+//  Created by 沈清昊 on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
